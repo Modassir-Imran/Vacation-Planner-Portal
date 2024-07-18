8f4c2cd6c2493fbba3d43e02099b2fc5688cc3ac
Modassir Imran <modassirimran904@gmail.com>
2024-07-18 12:19:39 +0530

    update changes

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -4,7 +4,7 @@ function Tours({tours,removeTour}) {
     return(
         <div className='container'>
             <div>
-                <h2 className='title'>Plan with Love</h2>
+                <h2 className='title'>Planning for Tours</h2>
 
             </div>
             <div className='cards'>
